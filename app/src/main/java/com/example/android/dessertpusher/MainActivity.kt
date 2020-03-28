057a8862e51f6b126d85b6ed085735d4f76fae7a
Damian <damian.ogorek.1996@gmail.com>
2020-03-28 15:43:29 +0100

    Added Lifecycle Observation.

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -77,7 +77,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
-        dessertTimer = DessertTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -156,12 +156,10 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
-        dessertTimer.startTimer()
         Timber.i("onStart called")
     }
 
     override fun onStop() {
         super.onStop()
-        dessertTimer.stopTimer()
     }
 }
